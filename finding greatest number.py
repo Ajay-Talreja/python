64dc7aa9ec9ad566f07d9697d5936758a4e1175f
Ajay-Talreja <talreja.ajay10@gmail.com>
2016-10-21 09:07:40 +0530

    Add files via upload

---
+++ finding greatest number.py
@@ -0,0 +1,8 @@
+x=int(input("enter a number 1:"))
+y=int(input("enter a number 2:" ))
+if x < y:
+      print("number 2 is greater")
+elif x > y:
+      print("number 1 is greater")
+else:
+      print("equal")
